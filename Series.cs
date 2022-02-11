7ba1847e7aa94b1a097d9f8ef1c9d7b48d08a757
MukulSingh7410 <99354096+MukulSingh7410@users.noreply.github.com>
2022-02-11 16:02:59 +0530

    Create Series.cs

---
+++ Series.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class SeriesProgram
+{ 
+	public static void Main (string [ ] args)
+	{ 
+		
+		int i,num=12;
+		Console.Write("Enter the Number :");
+		
+		for (i=1;i<5;i++) 
+		
+		{ num = num-1;
+		
+		Console.Write(" num ");
+		
+		num = num+2;
+		
+		Console.Write(num);
+		
+		}
+		
+		
+		}
+		
+		}
+		
+    
